e8bd07d6b3fd3b964db29ffd8c6a426b6289c497
yuno-kojo <peoplecanfly256@gmail.com>
2016-11-15 18:04:14 +0300

    11.1

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo nginx -t && sudo /etc/init.d/nginx restart
-sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
